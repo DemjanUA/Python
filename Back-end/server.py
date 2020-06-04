c11f297dbb25437169bcaa005dc5a2e7dfed214c
Dmytro Uchkin <demjan@Demjans-MacBook-Pro.local>
2020-06-04 14:01:38 +0300

    lfs init

--- Back-end/server.py
+++ Back-end/server.py
@@ -3,8 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return {
-    "username": user.username,
-    "theme": user.theme,
-    "image": url_for("user_image", filename=user.image),
-  }
+  return 'Hello'
