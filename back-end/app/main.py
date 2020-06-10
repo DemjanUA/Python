c91b68e22cbb49ceb84864d0298e6297ffe51c54
Dmytro Uchkin <demjan@Demjans-MacBook-Pro.local>
2020-06-10 17:41:34 +0300

    file fixes

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -1,7 +1,3 @@
-# In[1]:
-# export FLASK_APP=server.py
-
-
 import sys
 import numpy
 import imageio
@@ -9,11 +5,11 @@ import matplotlib.pyplot
 from flask import Flask, json, request
 from flask_cors import CORS
 
-corePath = '../_core/'
+corePath = '_core/'
 
 
 sys.path.append(corePath)
-from _classes import neuralNetwork
+from ._classes import neuralNetwork
 
 
 # In[2]:
@@ -31,39 +27,13 @@ learning_rate = 0.1
 n = neuralNetwork(input_nodes,hidden_nodes,output_nodes, learning_rate)
 n.load()
 
-
-# In[3]:
-
-
-# img_array = imageio.imread(corePath + 'images/2828_my_own_4.png', as_gray=True)
-    
-# # reshape from 28x28 to list of 784 values, invert values
-# img_data  = 255.0 - img_array.reshape(784)
-    
-# # then scale data to range from 0.01 to 1.0
-# img_data = (img_data / 255.0 * 0.99) + 0.01
-# print("min = ", numpy.min(img_data))
-# print("max = ", numpy.max(img_data))
-
-# # plot image
-# matplotlib.pyplot.imshow(img_data.reshape(28,28), cmap='Greys', interpolation='None')
-
-# # query the network
-# outputs = n.query(img_data)
-# print (outputs)
-
-# # the index of the highest value corresponds to the label
-# label = numpy.argmax(outputs)
-# print("network says ", label)
-
-
 # In[4]:
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/', methods=['POST'])
-def hello_world():
+def home_view():
   testData = numpy.array(request.get_json())
 
   testData  = 255.0 - testData
